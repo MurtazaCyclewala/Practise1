63c9781eab57131b9a50a68c6f634015b78c8417
Murtaza Cyclewala <80732148+MurtazaCyclewala@users.noreply.github.com>
2022-03-25 20:52:36 +0300

    few minor changes + addition of uploading

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, url_for, request, session, redirect, f
 import flask_pymongo
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
-from flask.ext.pymongo import PyMongo
+from flask_pymongo import PyMongo
 import bcrypt
 
 app = Flask(__name__)
@@ -19,7 +19,7 @@ def index():
     if 'username' in session:
         return 'You are logged in as ' + session['username']
 
-    return render_template('Login.html')
+    return render_template('signup.html')
 
 
 @app.route('/signup', methods=['POST', 'GET'])
@@ -40,7 +40,7 @@ def signup():
 
 
 """@app.route('/')"""
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST','GET'])
 def login():
     users = mongo.db.users
     login_user = users.find_one({'name' : request.form['username']})
@@ -51,6 +51,46 @@ def login():
             return redirect(url_for('index'))
 
     return 'Invalid username/password combination'
+
+
+
+
+
+UPLOAD_FOLDER = 'static/uploads/'
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+ 
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+ 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #print('upload_image filename: ' + filename)
+        flash('Image successfully uploaded and displayed below')
+        return render_template('index.html', filename=filename)
+    else:
+        flash('Allowed image types are - png, jpg, jpeg, gif')
+        return redirect(request.url)
+ 
+@app.route('/upload/display/<filename>')
+def display_image(filename):
+    #print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
+ 
+
 
 
 if __name__ == "__main__":
