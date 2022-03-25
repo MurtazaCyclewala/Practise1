73bcdceb12ec7d22c3de4177f7ef48829f873e7d
Murtaza Cyclewala <80732148+MurtazaCyclewala@users.noreply.github.com>
2022-03-25 20:34:38 +0300

    few errors correction

--- app.py
+++ app.py
@@ -1,27 +1,31 @@
+import flask
 from flask import Flask, render_template, url_for, request, session, redirect, flash
-from flask_pymongo import PyMongo
+import flask_pymongo
+from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
+from flask.ext.pymongo import PyMongo
 import bcrypt
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'testing'
-app.config['MONGO_dbname'] = 'Basic_auth'
+#app.config['SECRET_KEY'] = 'testing'
+app.config['MONGO_DBNAME'] = 'Basic_auth'
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/Basic_auth'
 mongo = PyMongo(app)
 
 
-@app.route('/index')
+@app.route('/')
 def index():
     if 'username' in session:
         return 'You are logged in as ' + session['username']
 
-    return render_template('index.html')
+    return render_template('Login.html')
 
 
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
     if request.method == 'POST':
-        users = mongo.db.Mongo
+        users = mongo.db.users2
         existing_user = users.find_one({'name' : request.form['username']})
 
         if existing_user is None:
@@ -32,17 +36,17 @@ def signup():
         
         return 'That username already exists!'
 
-    return render_template('signup.html')
+    return render_template('SignUp.html')
 
 
-@app.route('/')
+"""@app.route('/')"""
 @app.route('/login', methods=['POST'])
 def login():
     users = mongo.db.users
     login_user = users.find_one({'name' : request.form['username']})
 
     if login_user:
-        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password']) == login_user['password']:
             session['username'] = request.form['username']
             return redirect(url_for('index'))
 
