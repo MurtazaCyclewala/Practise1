2592526c250010960cc18aad673c7013c86e58af
Murtaza Cyclewala <80732148+MurtazaCyclewala@users.noreply.github.com>
2022-03-25 18:55:33 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, url_for, request, session, redirect, flash
+from flask_pymongo import PyMongo
+import bcrypt
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'testing'
+app.config['MONGO_dbname'] = 'Basic_auth'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/Basic_auth'
+mongo = PyMongo(app)
+
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return 'You are logged in as ' + session['username']
+
+    return render_template('index.html')
+
+
+@app.route('/signup', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        users = mongo.db.Mongo
+        existing_user = users.find_one({'name' : request.form['username']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name' : request.form['username'], 'password' : hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        
+        return 'That username already exists!'
+
+    return render_template('signup.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    users = mongo.db.users
+    login_user = users.find_one({'name' : request.form['username']})
+
+    if login_user:
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+
+    return 'Invalid username/password combination'
+
+
+if __name__ == "__main__":
+    app.run(host = "localhost",port =5000,debug = True)
